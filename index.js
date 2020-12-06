d2c096dccd526610b856293ceb0eb5c4420c4860
Igor Srdoč <igor@srdoc.si>
2020-12-06 02:37:16 +0100

    16. Parsing Query Strings

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const server = http.createServer((request, response) => {
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
   const method = request.method.toUpperCase()
+  const queryStringObject = parsedUrl.query
 
   response.end('Hello!\n')
 
   console.log(`Request received on path: ${trimmedPath} and method: ${method}`)
+  console.log(queryStringObject)
 })
 
 server.listen(3000, () => {
