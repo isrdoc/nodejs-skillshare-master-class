e20a8fd645a1a6c9a0a1f98ea39bbca0d33c0590
Igor Srdoč <igor@srdoc.si>
2020-12-06 01:28:12 +0100

    13. Starting a Server

---
+++ index.js
@@ -0,0 +1,9 @@
+import http from 'http'
+
+const server = http.createServer((request, response) => {
+    response.end('Hello!\n')
+})
+
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000 now')
+})
