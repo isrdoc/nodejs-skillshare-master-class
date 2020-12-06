c020943c69951b38a6aa0a6cd6e8b2808d4367a8
Igor Srdoč <igor@srdoc.si>
2020-12-06 01:37:52 +0100

    14. Parsing Request Paths

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import http from 'http'
+import url from 'url'
 
 const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true)
+    const path = parsedUrl.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
     response.end('Hello!\n')
+
+    console.log(`Request received on path: ${trimmedPath}`)
 })
 
 server.listen(3000, () => {
