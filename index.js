0928f5eb532c9199b1fd9b00ee0f41c07d85cb72
Igor Srdoč <igor@srdoc.si>
2020-12-06 02:27:32 +0100

    15. Parsing HTTP Methods

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import http from 'http'
 import url from 'url'
 
 const server = http.createServer((request, response) => {
-    const parsedUrl = url.parse(request.url, true)
-    const path = parsedUrl.pathname
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  const parsedUrl = url.parse(request.url, true)
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  const method = request.method.toUpperCase()
 
-    response.end('Hello!\n')
+  response.end('Hello!\n')
 
-    console.log(`Request received on path: ${trimmedPath}`)
+  console.log(`Request received on path: ${trimmedPath} and method: ${method}`)
 })
 
 server.listen(3000, () => {
-    console.log('The server is listening on port 3000 now')
+  console.log('The server is listening on port 3000 now')
 })
